0e3f30127abc272b35ec6f0cb8dbf73215c48d7a
Hatapoi <mhatta217@gmail.com>
2025-02-09 21:57:49 +0700

    data(poli): add repository

---
+++ src/Poli/poli.repository.js
@@ -0,0 +1,48 @@
+const prisma = require("../db/index");
+
+const allData = async() => {
+    const poli = await prisma.poli.findMany();
+
+    return poli;
+}
+
+const allDataById = async(id) => {
+    const poli = await prisma.poli.findUnique({
+        where: { id }
+    })
+
+    return poli;
+}
+
+const createData = async() => {
+    const poli = await prisma.poli.create({
+        data: {
+            nama: newPoli.nama
+        }
+    });
+
+    return poli;
+}
+
+const updateData = async(id, nama) => {
+    const poli = await prisma.poli.update({
+        where: { id },
+        data: { nama },
+    })
+
+    return poli;
+} 
+
+const deleteData = async(id) => {
+    const deletePoli = await prisma.poli.delete({ where: { id } })
+
+    return deletePoli;
+}
+
+module.exports = {
+    allData,
+    allDataById,
+    createData,
+    updateData,
+    deleteData,
+}
--- src/Poli/poli.service.js
+++ src/Poli/poli.service.js
@@ -1,40 +1,32 @@
-const prisma = require("../db/index");
+const { allData, allDataById, createData, updateData, deleteData } = require("./poli.repository");
+
 
 const getAllPoli = async() => {
-    const poli = await prisma.poli.findMany()
+    const poli = await allData();
 
     return poli;
 }
 
 const getPoliById = async(id) => {
-    const poli = await prisma.poli.findUnique({
-        where: { id }
-    })
+    const poli = await allDataById(id);
 
     return poli;
 }
 
 const addPoli = async() => {
-    const poli = await prisma.poli.create({
-        data: {
-            nama: newPoli.nama
-        }
-    });
+    const poli = await createData();
 
     return poli;
 }
 
 const updatePoli = async(id, nama) => {
-    const poli = await prisma.poli.update({
-        where: { id },
-        data: { nama },
-    })
+    const poli = await updateData(id, nama);
 
     return poli;
 }
 
 const deletePoli = async() => {
-    const deletePoli = await prisma.poli.delete({ where: { id } })
+    const deletePoli = await deleteData(id);
 
     return deletePoli;
 }
