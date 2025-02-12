c375efe37f9c378f24514dcbf52c0ec5cb8b620d
Hatapoi <mhatta217@gmail.com>
2025-02-12 11:09:39 +0700

    data(historyPenanganan): add fondation

---
+++ src/HistoryPenanganan/hPenanganan.controller.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const prisma = require("../db/index");
+
+const router = express.Router();
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -12,11 +12,13 @@ const poliController = require("./Poli/poli.controller");
 const penangananController = require("./Penanganan/penanganan.controller");
 const userController = require("./User/user.controller");
 const historyController = require("./History/history.controller");
+const historyPenanganan = require("./HistoryPenanganan/hPenanganan.controller");
 
 app.use("/poli", poliController)
 app.use("/penanganan", penangananController);
 app.use("/user", userController);
 app.use("/history", historyController);
+app.use("/hpenanganan", historyPenanganan);
 
 app.listen(PORT, () => {
     console.log(`express API running on ${PORT}`)
